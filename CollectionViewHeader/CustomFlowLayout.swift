d03dab6ce038b7df81743706a76e912f0c4b5756
Michael Miles <michael.miles@seasoned.co>
2022-03-24 13:55:23 -0400

    Link new custom layout to collection view

---
+++ CollectionViewHeader/CustomFlowLayout.swift
@@ -0,0 +1,12 @@
+//
+//  CustomFlowLayout.swift
+//  CollectionViewHeader
+//
+//  Created by Michael Miles on 3/24/22.
+//
+
+import UIKit
+
+class CustomFlowLayout: UICollectionViewFlowLayout {
+
+}
